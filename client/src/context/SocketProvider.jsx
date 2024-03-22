c60cf29feb40bb6eae4b8140733d2555257817d2
avik <aviksain007@gmail.com>
2024-03-22 12:21:31 +0530

    updated the backend url

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:3001"), []);
+  const socket = useMemo(() => io(import.meta.env.VITE_URL), []);
 
   return (
     <SocketContext.Provider value={socket}>
